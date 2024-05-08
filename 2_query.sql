150e337d1aeff4a15478372e676133a33c8680e4
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-08 16:08:23 +0200

    add formulated answers

--- 2_query.sql
+++ 2_query.sql
@@ -3,3 +3,6 @@ FROM `sql-sandbox-20240505.lifeexpectancy.lifeexpectancy`
 WHERE MetricObserved = "Life expectancy at birth (years)"
 GROUP BY RegionDisplay
 ORDER BY Average_Life_Expectancy DESC
+
+-- Is Europe the region with the highest life expectancy at birth? -> yes.
+-- Which region has the lowest life expectancy at birth? -> Africa
