f3f81ee4a3ffa21c838c986d5be9965508bbc7bf
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-08 16:41:09 +0200

    add answer to question 8

---
+++ 8_question.sql
@@ -0,0 +1,3 @@
+-- With reference to the last query (last question), look at the country that consumed the highest quantity of alcohol overall, was it the first ranking country in terms of beer, wine or spirits? If not, was it in the top 10 of any of those 3 rankings (beer, wine or spirits)?
+
+-- Denmark is never the topmost country, and only in the section of wine at position 5
