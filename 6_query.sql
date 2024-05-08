901e94a4ac89b5f7cd1e9db9a04785102474c52b
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-08 16:39:25 +0200

    add result tables to question 6

--- 6_query.sql
+++ 6_query.sql
@@ -6,6 +6,18 @@ GROUP BY CountryDisplay
 ORDER BY avg_beer_servings DESC
 LIMIT 10;
 
+-- CountryDisplay	avg_spirit_servings
+-- Grenada	438.0
+-- Belarus	373.0
+-- Haiti	326.0
+-- Russian Federation	326.0
+-- St. Lucia	315.0
+-- Guyana	302.0
+-- Slovakia	293.0
+-- Dominica	286.0
+-- Thailand	258.0
+-- Cook Islands	254.0
+
 SELECT
 	CountryDisplay,
 	AVG(spirit_servings) AS avg_spirit_servings
@@ -14,6 +26,18 @@ GROUP BY CountryDisplay
 ORDER BY avg_spirit_servings DESC
 LIMIT 10;
 
+-- CountryDisplay	avg_spirit_servings
+-- Grenada	438.0
+-- Belarus	373.0
+-- Haiti	326.0
+-- Russian Federation	326.0
+-- St. Lucia	315.0
+-- Guyana	302.0
+-- Slovakia	293.0
+-- Dominica	286.0
+-- Thailand	258.0
+-- Cook Islands	254.0
+
 SELECT
 	CountryDisplay,
 	AVG(wine_servings) AS avg_wine_servings
@@ -21,3 +45,15 @@ FROM `sql-sandbox-20240505.lifeexpectancy.lifeexpectancy`
 GROUP BY CountryDisplay
 ORDER BY avg_wine_servings DESC
 LIMIT 10;
+
+-- CountryDisplay	avg_wine_servings
+-- France	370.0
+-- Portugal	339.0
+-- Andorra	312.0
+-- Switzerland	280.0
+-- Denmark	278.0
+-- Slovenia	276.0
+-- Luxembourg	271.0
+-- Croatia	254.0
+-- Italy	237.0
+-- Equatorial Guinea	233.0
