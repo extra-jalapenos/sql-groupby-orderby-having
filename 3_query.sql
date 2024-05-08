de9eff3013c529706b27e7d4ddf8a4a8f267e1cc
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-08 16:13:06 +0200

    add solution for highest life expectancy of a country at 60

--- 3_query.sql
+++ 3_query.sql
@@ -4,6 +4,16 @@ SELECT  RegionDisplay, AVG(Numeric) AS Average_Life_Expectancy
 FROM `sql-sandbox-20240505.lifeexpectancy.lifeexpectancy`
 WHERE MetricObserved = "Life expectancy at age 60 (years)"
 GROUP BY RegionDisplay
-ORDER BY Average_Life_Expectancy DESC
+ORDER BY Average_Life_Expectancy DESC;
 
 -- -> no, it's the Americas
+
+-- Which country has the highest life expectancy after 60?
+
+SELECT CountryDisplay, AVG(Numeric) AS Average_Life_Expectancy
+FROM `sql-sandbox-20240505.lifeexpectancy.lifeexpectancy`
+WHERE MetricObserved = "Life expectancy at age 60 (years)"
+GROUP BY CountryDisplay
+ORDER BY Average_Life_Expectancy DESC;
+
+-- it's Japan at 24.91 yrs
