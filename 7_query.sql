1e3c2870a9eb9f967d9f0be9e474fa6905f5175a
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-08 16:36:57 +0200

    add top 10 alcohol consumption countries

---
+++ 7_query.sql
@@ -0,0 +1,7 @@
+SELECT
+	CountryDisplay,
+	AVG(beer_servings) + AVG(spirit_servings) + AVG(wine_servings) AS avg_alcohol_servings
+FROM `sql-sandbox-20240505.lifeexpectancy.lifeexpectancy`
+GROUP BY CountryDisplay
+ORDER BY avg_alcohol_servings DESC
+LIMIT 10;
