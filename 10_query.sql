fbac95c4e3a6a8d3fccb82d1c3a0ccc97953b185
AllyDouillette <142409831+AllyDouillette@users.noreply.github.com>
2024-05-08 16:52:42 +0200

    add written answer / question

--- 10_query.sql
+++ 10_query.sql
@@ -1,3 +1,5 @@
+-- which country has the highest gap (in terms of years) between females and males?
+
 SELECT
 	CountryDisplay,
 	AVG(CASE WHEN SexDisplay = "Female" THEN Numeric END) - AVG(CASE WHEN SexDisplay = "Male" THEN Numeric END) AS avg_LE_delta_gender
@@ -6,3 +8,5 @@ WHERE MetricObserved = "Life expectancy at birth (years)"
 GROUP BY CountryDisplay
 ORDER BY avg_LE_delta_gender DESC
 LIMIT 1;
+
+-- it's the Russian Federation with 11.95 years
